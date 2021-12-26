0ed3dadc58b0ed567fbd8ffea8aa618f73662c91
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-12-26 22:45:29 +0300

    Print bit sequences in chunks of 8

--- ch05/src/bin/endianness.rs
+++ ch05/src/bin/endianness.rs
@@ -1,5 +1,15 @@
 use std::mem::transmute;
 
+fn to_chunks_of_8_bit(num: i32) -> String {
+    let bits = format!("{:b}", num);
+    bits.chars()
+        .collect::<Vec<char>>()
+        .chunks(8)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
 fn main() {
     let big_endian: [u8; 4] = [0xAA, 0xBB, 0xCC, 0xDD];
     let little_endian: [u8; 4] = [0xDD, 0xCC, 0xBB, 0xAA];
@@ -7,5 +17,11 @@ fn main() {
     let a: i32 = unsafe { transmute(big_endian) };
     let b: i32 = unsafe { transmute(little_endian) };
 
+    println!("0xAA = {:b}", 0xAA);
+
+    println!("a    = {}", to_chunks_of_8_bit(a));
+    println!("b    = {}", to_chunks_of_8_bit(b));
+
+    println!();
     println!("{} vs {}", a, b);
 }
